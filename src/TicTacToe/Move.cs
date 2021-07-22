ddc7383bc1c04ce0f06fb9c94fdb109f737e492e
asdia0 <asdia.the.potato@gmail.com>
2021-07-22 13:54:35 +0800

    Add Move

---
+++ src/TicTacToe/Move.cs
@@ -0,0 +1,29 @@
+﻿namespace TicTacToe
+{
+    /// <summary>
+    /// Defines a move.
+    /// </summary>
+    public struct Move
+    {
+        /// <summary>
+        /// Gets the player that made the <see cref="Move"/>.
+        /// </summary>
+        public Player Player { get; }
+
+        /// <summary>
+        /// Gets the position of the <see cref="Move"/> made.
+        /// </summary>
+        public Position Position { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Move"/> struct.
+        /// </summary>
+        /// <param name="player">The player that made the <see cref="Move"/>.</param>
+        /// <param name="position">The position of the <see cref="Move"/> made.</param>
+        public Move(Player player, Position position)
+        {
+            this.Player = player;
+            this.Position = position;
+        }
+    }
+}
