f4bedaf4093f516c1ea95282ce031f0bc69417f4
asdia0 <asdia.the.potato@gmail.com>
2021-08-13 18:13:05 +0800

    Add TicTacToeException

---
+++ src/TicTacToe/TicTacToeException.cs
@@ -0,0 +1,24 @@
+﻿namespace TicTacToe
+{
+    using System;
+
+    /// <summary>
+    /// Defines an exception thrown in this project.
+    /// </summary>
+    public class TicTacToeException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TicTacToeException"/> class.
+        /// </summary>
+        public TicTacToeException()
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TicTacToeException"/> class.
+        /// </summary>
+        /// <param name="message">A message about the exception.</param>
+        public TicTacToeException(string message)
+            : base(message)
+        { }
+    }
+}
