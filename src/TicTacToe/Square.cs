e0966c180d2d4ea6e9cbbac8c2a0da666a5fa3df
asdia0 <asdia.the.potato@gmail.com>
2021-08-13 18:28:39 +0800

    Add Square.ID

--- src/TicTacToe/Square.cs
+++ src/TicTacToe/Square.cs
@@ -1,6 +1,7 @@
 ﻿namespace TicTacToe
 {
-    using System;
+    using System.Linq;
+    using static System.Math;
 
     /// <summary>
     /// Represents a square on a <see cref="TicTacToe.Grid"/>.
@@ -8,9 +9,55 @@
     public class Square
     {
         /// <summary>
-        /// Gets or sets the <see cref="Square"/>'s <see cref="TicTacToe.Position"/> on the <see cref="TicTacToe.Grid"/>.
+             /// Gets or sets the <see cref="Square"/>'s unique identification number.
+             /// </summary>
+        public int ID
+        {
+            get
+            {
+                return this._ID;
+            }
+
+            set
+            {
+                if (this.IDSet)
+                {
+                    if (value >= this.Grid.Length * this.Grid.Breadth)
+                    {
+                        throw new TicTacToeException("Square ID too big.");
+                    }
+                    else if (value < 0)
+                    {
+                        throw new TicTacToeException("Square ID too small.");
+                    }
+
+                    this._ID = value;
+                }
+
+                this.IDSet = false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Square"/>'s <see cref="TicTacToe.Position"/> on the <see cref="TicTacToe.Grid"/>.
         /// </summary>
-        public Position Position { get; set; }
+        public Position Position
+        {
+            get
+            {
+                int x = (this.ID % this.Grid.Length) + 1;
+                int y = (int)Floor((double)(this.ID / this.Grid.Length)) + 1;
+
+                return new Position(x, y);
+            }
+        }
+
+        /// <summary>
+        /// Value determining whether <see cref="ID"/> has been set.
+        /// </summary>
+        private bool IDSet = false;
+
+        private int _ID;
 
         /// <summary>
         /// Gets or sets the <see cref="TicTacToe.Grid"/> the <see cref="Square"/> is on.
@@ -37,6 +84,8 @@
                     throw new TicTacToeException("Square already exists.");
                 }
             }
+
+            this.ID = grid.Squares.OrderBy(i => i.ID).Last().ID++;
 
             this.Grid = grid;
             this.Position = position;
