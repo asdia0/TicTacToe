c237fe1e79d1f84a8b7b60e3412142377cf3657c
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 04:52:49 +0800

    Add TicTacToe.Example

---
+++ src/TicTacToe.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTacToe.Example
+{
+    using System;
+    using TicTacToe;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
