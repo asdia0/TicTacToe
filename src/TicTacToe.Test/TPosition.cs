0dbfb22e10cc974087f652a1ac4d021f52f0465f
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 18:52:22 +0800

    Update TPosition

--- src/TicTacToe.Test/TPosition.cs
+++ src/TicTacToe.Test/TPosition.cs
@@ -1,13 +1,79 @@
 namespace TicTacToe.Test
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+    /// <summary>
+    /// Tests <see cref="Position"/>.
+    /// </summary>
     [TestClass]
     public class TPosition
     {
+        /// <summary>
+        /// Tests if <see cref="Position.X"/> is correctly assigned.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void X()
         {
+            int length = 3;
+            int breadth = 3;
+            Grid grid = new(length, breadth);
+
+            List<int> expected = new()
+            {
+                1,
+                2,
+                3,
+                1,
+                2,
+                3,
+                1,
+                2,
+                3,
+            };
+
+            CollectionAssert.AreEqual(expected, grid.Squares.Select(i => i.Position.X).ToList());
+        }
+
+        /// <summary>
+        /// Tests if <see cref="Position.Y"/> is correctly assigned.
+        /// </summary>
+        [TestMethod]
+        public void Y()
+        {
+            int length = 3;
+            int breadth = 3;
+            Grid grid = new(length, breadth);
+
+            List<int> expected = new()
+            {
+                1,
+                1,
+                1,
+                2,
+                2,
+                2,
+                3,
+                3,
+                3,
+            };
+
+            CollectionAssert.AreEqual(expected, grid.Squares.Select(i => i.Position.Y).ToList());
+        }
+
+        /// <summary>
+        /// Tests if <see cref="Position.Equals(object)"/> is correct.
+        /// </summary>
+        [TestMethod]
+        public void Equal()
+        {
+            Position original = new(1, 1);
+            Position clone = new(original.X, original.Y);
+            Position fake = new(original.X, original.Y + 1);
+
+            Assert.AreEqual(true, original.Equals(clone));
+            Assert.AreEqual(false, original.Equals(fake));
         }
     }
 }
