5d6c67b85f4739d7c2bc2ff0cd9ab8ea833c3a30
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:16:11 +0800

    Add TMove

---
+++ src/TicTacToe.Test/TMove.cs
@@ -0,0 +1,13 @@
+namespace TicTacToe.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TMove
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
