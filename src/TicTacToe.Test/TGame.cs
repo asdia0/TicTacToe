c62cd36d309ccf2a8c94fd151f54694327963230
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:15:29 +0800

    Add TGame

--- src/TicTacToe.Test/TGame.cs
+++ src/TicTacToe.Test/TGame.cs
@@ -1,9 +1,9 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 namespace TicTacToe.Test
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     [TestClass]
-    public class UnitTest1
+    public class TGame
     {
         [TestMethod]
         public void TestMethod1()
