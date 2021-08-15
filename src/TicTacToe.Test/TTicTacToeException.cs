acf2135098c3c78988cf91f7b847a8218e1067c0
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:29:32 +0800

    Update TTicTacToeException

--- src/TicTacToe.Test/TTicTacToeException.cs
+++ src/TicTacToe.Test/TTicTacToeException.cs
@@ -1,13 +1,53 @@
 namespace TicTacToe.Test
 {
+    using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TicTacToe;
 
+    /// <summary>
+    /// Tests <see cref="TicTacToeException"/>.
+    /// </summary>
     [TestClass]
     public class TTicTacToeException
     {
+        /// <summary>
+        /// Tests if <see cref="TicTacToeException"/> throws an <see cref="Exception"/>.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void ThrowTicTacToeException()
         {
+            bool succeeded = false;
+
+            try
+            {
+                throw new TicTacToeException();
+            }
+#pragma warning disable
+            catch (TicTacToeException ex)
+#pragma warning restore
+            {
+                succeeded = true;
+            }
+
+            Assert.AreEqual(true, succeeded);
+        }
+
+        /// <summary>
+        /// Tests if <see cref="TicTacToeException"/> has the correct message.
+        /// </summary>
+        [TestMethod]
+        public void ThrowTicTacToeExceptionWithMessage()
+        {
+            string expected = "This is a message.";
+
+            try
+            {
+                throw new TicTacToeException(expected);
+            }
+            catch (TicTacToeException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
         }
     }
 }
