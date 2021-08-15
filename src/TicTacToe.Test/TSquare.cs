5f3e6147c2cab26cfdec415cc9e9b24095843a5e
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:17:25 +0800

    Add TSquare

---
+++ src/TicTacToe.Test/TSquare.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacToe.Test
+{
+    [TestClass]
+    public class TSquare
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
