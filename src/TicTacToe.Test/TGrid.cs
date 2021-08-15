ce42cc5c86f9140ef49b605de76373e9501123e0
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:15:47 +0800

    Add TGrid

---
+++ src/TicTacToe.Test/TGrid.cs
@@ -0,0 +1,13 @@
+namespace TicTacToe.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TGrid
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
