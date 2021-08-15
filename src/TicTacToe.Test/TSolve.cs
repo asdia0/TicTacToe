9b5b61fc9ddd655fd95c184d0025479c71e82fa7
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:17:06 +0800

    Add TSolve

---
+++ src/TicTacToe.Test/TSolve.cs
@@ -0,0 +1,13 @@
+namespace TicTacToe.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TSolve
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
