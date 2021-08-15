8264d80f9b742867f155efbf359d4d6d20860ac0
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 18:55:13 +0800

    Update TMove

--- src/TicTacToe.Test/TMove.cs
+++ src/TicTacToe.Test/TMove.cs
@@ -2,12 +2,50 @@ namespace TicTacToe.Test
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+    /// <summary>
+    /// Tests <see cref="Move"/>.
+    /// </summary>
     [TestClass]
     public class TMove
     {
+        /// <summary>
+        /// Tests if <see cref="Move.Player"/> is assigned correctly.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void Player()
         {
+            int length = 3;
+            int breadth = 3;
+            Grid grid = new(length, breadth);
+            int players = 2;
+            int toWin = 3;
+            Game game = new(grid, players, toWin);
+
+            Position toPlay = new(1, 1);
+
+            game.Play(toPlay);
+
+            Assert.AreEqual(0, game.MoveList[0].Player);
+        }
+
+        /// <summary>
+        /// Tests if <see cref="Move.Position"/> is assigned correctly.
+        /// </summary>
+        [TestMethod]
+        public void Position()
+        {
+            int length = 3;
+            int breadth = 3;
+            Grid grid = new(length, breadth);
+            int players = 2;
+            int toWin = 3;
+            Game game = new(grid, players, toWin);
+
+            Position toPlay = new(1, 1);
+
+            game.Play(toPlay);
+
+            Assert.AreEqual(toPlay, game.MoveList[0].Position);
         }
     }
 }
