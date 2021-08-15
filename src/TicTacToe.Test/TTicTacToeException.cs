29fa76952048dfc1527505c964dc8207bd4304db
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:17:43 +0800

    Add TTicTacToeException

---
+++ src/TicTacToe.Test/TTicTacToeException.cs
@@ -0,0 +1,13 @@
+namespace TicTacToe.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TTicTacToeException
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
