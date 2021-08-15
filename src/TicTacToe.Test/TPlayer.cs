9e4b14a328e7815cf4b2e789e9de7a9f3d49c865
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 14:16:33 +0800

    Add TPlayer

---
+++ src/TicTacToe.Test/TPlayer.cs
@@ -0,0 +1,13 @@
+namespace TicTacToe.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TPlayer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
