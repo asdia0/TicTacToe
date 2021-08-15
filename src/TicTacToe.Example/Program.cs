f1d81a45e4a99066ecad517f25a64687d36dc914
asdia0 <asdia.the.potato@gmail.com>
2021-08-15 23:45:49 +0800

    Create sample program

--- src/TicTacToe.Example/Program.cs
+++ src/TicTacToe.Example/Program.cs
@@ -1,13 +1,53 @@
 ﻿namespace TicTacToe.Example
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using TicTacToe;
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // TODO: Implement hash table for various depths
+            int length = 3;
+            int breadth = 3;
+            Grid grid = new(length, breadth);
+            int players = 2;
+            int toWin = 3;
+            Game game = new(grid, players, toWin);
+
+            while (!game.IsOver)
+            {
+                if (game.Turn == 0)
+                {
+                    Position toPlay = (Position)Solve.FindBestMove(game, game.Grid.Squares.Where(i => i.Player == null).Count()).Position;
+                    game.Play(toPlay);
+                    Console.WriteLine($"I have played {toPlay}.");
+                    Console.WriteLine(game.Grid);
+                }
+                else
+                {
+                    Console.WriteLine("Enter the x-coordinate: ");
+                    string stringX = Console.ReadLine();
+                    int x = int.Parse(stringX);
+                    Console.WriteLine("Enter the y-coordinate: ");
+                    string stringY = Console.ReadLine();
+                    int y = breadth + 1 - int.Parse(stringY);
+
+                    game.Play(new(x, y));
+                    Console.Write(game.Grid);
+                }
+            }
+
+            if (game.IsDraw)
+            {
+                Console.WriteLine("Draw!");
+            }
+            else
+            {
+                Console.WriteLine("Good game!");
+            }
         }
     }
 }
